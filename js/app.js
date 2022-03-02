3f3ffd5c6b70a9fea84718fde63c1be3ccd53f30
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-03-02 20:39:01 +0600

    finish

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// see more Button 
 function SeeMore(remove, add) {
   const seeMore = document.getElementById("seeMore");
   seeMore.classList.remove(remove);
@@ -13,8 +14,7 @@ function spinnerAdd(remove, add) {
 const search = document.getElementById('search-btn').addEventListener('click', () => {
   const searchValue = document.getElementById('search-field')
   
-
-
+// erroe Show 
   const error = document.getElementById('error')
   if (searchValue.value == '') {
     error.innerText = "Please type to something for searching"
@@ -22,22 +22,17 @@ const search = document.getElementById('search-btn').addEventListener('click', (
     error.innerText = "";
 
     spinnerAdd("d-none", "d-block");
+
+    //Api Source
     const apiSource = () => {
       const url = (`https://openapi.programming-hero.com/api/phones?search=${searchValue.value}`);
       fetch(url)
         .then((res) => res.json())
         .then((data) => searchResult(data))
-        .catch((error) => Error(error));
-    };
-    const Error = () => {
-      searchValue.value = "";
-      const noFund = document.getElementById("result-container");
-      noFund.textContent = '';
-      alert('serch Result not Found')
-
     };
     apiSource();
    
+    // get Api Serach Result 
     const searchResult = (result) => {
       const getResult = result.data.slice(0, 20); 
       if(getResult.length===20){
@@ -46,18 +41,10 @@ const search = document.getElementById('search-btn').addEventListener('click', (
       else{
         SeeMore( "d-block","d-none")
       }
-      
-
-
-
-
       const displayShow = document.getElementById('result-container')
 
       spinnerAdd("d-block", "d-none");
-
-
       let resultCount = document.getElementById("count");
-
       if (getResult.length > 1) {
         resultCount.innerHTML = `${getResult.length} results found for "<strong>${searchValue.value}</strong>"`;
       } else if (getResultt.length == 1) {
@@ -67,9 +54,8 @@ const search = document.getElementById('search-btn').addEventListener('click', (
       searchValue.value = "";
       displayShow.textContent = "";
 
-
+      //loop Api Serach Result
       getResult.forEach(element => {
-        // console.log(element)
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `<div class="card shadow border-0 p-2">
@@ -87,12 +73,17 @@ const search = document.getElementById('search-btn').addEventListener('click', (
   }
 
 });
+
+// get Api Id 
 const phoneDetails = (phoneID) => {
   const url = `https://openapi.programming-hero.com/api/phone/${phoneID}`
   fetch(url)
     .then(res => res.json())
     .then((data) => popUpResult(data.data))
 };
+
+// set PopUp
+
 const popUpResult = popUpDetails => {
   const popUpshow = document.getElementById("popupDisplay");
   const div = document.createElement("div");
@@ -123,3 +114,8 @@ const popUpResult = popUpDetails => {
   popUpshow.textContent = "";
   popUpshow.appendChild(div);
 };
+
+
+function massage(){
+  alert('sorry bro, I can not fix this function, please tell me, how can fix this function, when I click see more button the all result show in display')
+}
