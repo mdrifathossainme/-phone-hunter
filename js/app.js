024f64f5c3f00d61555764dd3768052dc3cf7c5e
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-03-02 09:25:34 +0600

    spinner

--- js/app.js
+++ js/app.js
@@ -1,30 +1,38 @@
 const search = document.getElementById('search-btn').addEventListener('click', () => {
   const searchValue = document.getElementById('search-field')
-  const apiSource = () => {
-    const url = (`https://openapi.programming-hero.com/api/phones?search=${searchValue.value}`);
-    fetch(url)
-      .then(res => res.json())
-      .then(data => searchResult(data.data))
+
+  const error = document.getElementById('error')
+  if(searchValue.value ==''){
+    error.innerText="Please type to something for searching"
   }
-  apiSource();
-  const searchResult = (result) => {
-    const displayShow = document.getElementById('result-container')
-    result.forEach(element => {
-      // console.log(element)
-      const div = document.createElement('div')
-      div.classList.add('col')
-      div.innerHTML=`<div class="card shadow border-0 p-2">
-      <img src="${element.image}" class="card-img-top" alt="...">
-      <div class="card-body">
-          <h5 class="card-title">${element.brand}</h5>
-          <p class="card-text">${element.phone_name}</p>
-          <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#meal-details" onclick='phoneDetails("${element.slug}")'>More Details</button>
-      </div>
-      </div>      
-          `
-        displayShow.appendChild(div); 
-    });
+  else{
+    const apiSource = () => {
+      const url = (`https://openapi.programming-hero.com/api/phones?search=${searchValue.value}`);
+      fetch(url)
+        .then(res => res.json())
+        .then(data => searchResult(data.data))
+    }
+    apiSource();
+    const searchResult = (result) => {
+      const displayShow = document.getElementById('result-container')
+      result.forEach(element => {
+        // console.log(element)
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML=`<div class="card shadow border-0 p-2">
+        <img src="${element.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${element.brand}</h5>
+            <p class="card-text">${element.phone_name}</p>
+            <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#meal-details" onclick='phoneDetails("${element.slug}")'>More Details</button>
+        </div>
+        </div>      
+            `
+          displayShow.appendChild(div); 
+      });
+    }
   }
+  
 });
     const phoneDetails=(phoneID)=>{
       const url=`https://openapi.programming-hero.com/api/phone/${phoneID}`
@@ -37,19 +45,25 @@ const search = document.getElementById('search-btn').addEventListener('click', (
       const div = document.createElement("div");
       div.classList.add("row", "g-0", 'responsive');
       div.innerHTML = `
-          <div class="col-md-4" style="background-image:url('${popUpDetails.image}');background-repeat: no-repeat; background-position: center center; background-size: fit;">
-              <div style="height:300px"></div>
+          <div class="col-md-4" style="background-image:url('${popUpDetails.image}');background-repeat: no-repeat; background-position: center center; background-size: auto;">
+              <div style="height:auto"></div>
           </div>
           <div class="col-md-8">
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               <div class="card-body text-start">
                   <h3 class="card-title">Brand: <span>${popUpDetails.brand}</h3>
-                  <p>Model Name: ${popUpDetails.name}</p>
-                  <p>Storage: ${popUpDetails.mainFeatures.storage}</p>
-                  <p>${popUpDetails.name}</p>
-                  <p>${popUpDetails.name}</span><span>${popUpDetails.name}</p>
-              </div>
-          </div>
+                  <p>Model Name:<spsn class="releaseDate"> ${popUpDetails.name}</span></p>
+                  <p>DisplaySize: ${popUpDetails.mainFeatures.displaySize}</p>
+                  <p >chipSet: ${popUpDetails.mainFeatures.chipSet}</p> 
+                  <p>Memory: ${popUpDetails.mainFeatures.memory}</p>
+                  <p>Sensors: ${popUpDetails.mainFeatures.sensors}</p>
+                  <p> Bluetooth: ${popUpDetails.others.Bluetooth}</p>
+                  <p>GPS: ${popUpDetails.others.GPS}</p>
+                  <p>NFC: ${popUpDetails.others.NFC}</p>
+                  <p>Radio: ${popUpDetails.others.Radio}</p>
+                  <p>USB: ${popUpDetails.others.USB}</p>
+                  <p>WLAN: ${popUpDetails.others.WLAN}</p>
+                  <p>ReleaseDate:<spsn class="releaseDate"> ${popUpDetails.releaseDate ? popUpDetails.releaseDate:'Release date not available'} </spsn></p>    
       `;
     
       popUpshow.textContent = "";
